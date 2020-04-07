b75d53f5d97653582de7d742e5554a5e5f5b9c00
Takagi Kensuke <janus.wel.3@gmail.com>
2020-04-07 13:17:12 +0900

    :art: rewrite with CSS Modules

--- src/components/Panel/index.tsx
+++ src/components/Panel/index.tsx
@@ -1,15 +1,6 @@
 import React from 'react'
-
-const style = {
-  position: 'absolute' as const,
-  left: 0,
-  top: 0,
-  background: 'black',
-  opacity: 0.5,
-  width: '100vw',
-  height: '100vh',
-}
+import styles from './Panel.module.css'
 
 export default function Panel() {
-  return <div style={style} />
+  return <div className={styles.panel} />
 }
